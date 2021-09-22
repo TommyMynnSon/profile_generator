8e2e1f92df66aa3579a1c5c8a73279eb7f1ed38f
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-21 19:39:01 -0700

    Implement multiple questions and form descriptive paragraph in survey.js

---
+++ survey.js
@@ -0,0 +1,60 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Object to capture user inputted values.
+const details = {
+  name: '',
+  activity: '',
+  musicForActivity: '',
+  meal: '',
+  dishForMeal: '',
+  sport: '',
+  superpower: '',
+};
+
+// Question bank.
+const questions = [
+  "What's your name?: ",
+  "What's an activity you like doing?: ",
+  "What do you listen to while doing that?: ",
+  "Which meal is your favourite (eg: dinner, brunch, etc.)?: ",
+  "What's your favourite thing to eat for that meal?: ",
+  "Which sport is your absolute favourite?: ",
+  "What is your superpower?: ",
+];
+
+rl.question(questions[0], name => {
+  details.name = name;
+
+  rl.question(questions[1], activity => {
+    details.activity = activity;
+
+    rl.question(questions[2], musicForActivity => {
+      details.musicForActivity = musicForActivity;
+
+      rl.question(questions[3], meal => {
+        details.meal = meal;
+
+        rl.question(questions[4], dishForMeal => {
+          details.dishForMeal = dishForMeal;
+
+          rl.question(questions[5], sport => {
+            details.sport = sport;
+
+            rl.question(questions[6], superpower => {
+              details.superpower = superpower
+
+              console.log(`\n${details.name}'s Profile:\n- Likes ${details.activity} while listening to ${details.musicForActivity}\n- Favourite meal is ${details.meal} and favourite thing to eat for ${details.meal} is ${details.dishForMeal}\n- Favourite sport is ${details.sport}\n- Superpower is ${details.superpower}`);
+
+              rl.close();
+            })
+          })
+        })
+      })
+    })
+  })
+});
